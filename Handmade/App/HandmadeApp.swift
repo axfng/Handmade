e515b3e61fdda25d2dbf278b7a2c3c89a40ff1ec
axfng <alexlfeng22@gmail.com>
2024-11-07 22:44:06 -0500

    Initial Commit

---
+++ Handmade/App/HandmadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandmadeApp.swift
+//  Handmade
+//
+//  Created by alfeng on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct HandmadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
